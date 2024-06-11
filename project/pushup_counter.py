c912d3c28dc9e981dcabb1477c23bdfc5316c6ac
Rithvik <65542427+RITHVIK1101@users.noreply.github.com>
2024-06-10 20:54:58 -0700

    Update pushup_counter.py

--- project/pushup_counter.py
+++ project/pushup_counter.py
@@ -9,9 +9,9 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 def calculate_angle(a, b, c):
-    a = np.array(a) 
-    b = np.array(b) 
-    c = np.array(c)  
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
     
     radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
     angle = np.abs(radians * 180.0 / np.pi)
